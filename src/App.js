3583ad8689d985f871b17faee60e31dbb952e336
MohsenSami <mohsensami88@gmail.com>
2023-01-22 12:16:40 +0330

    show posts in home page by axios then catch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import Header from "./components/Header";
 
-function App() {
+const App = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    // const getData = async () => {
+    //     const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts?limit=4`);
+    //     setPosts(data);
+    // };
+    // useEffect(() => {
+    //     getData();
+    // }, []);
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+            setPosts(response.data);
+        });
+        setLoading(false);
+    }, []);
+
     return (
         <div className="App">
             <Header />
+            <div className="container">
+                {loading ? (
+                    <h1>Loading ....</h1>
+                ) : (
+                    <div className="row">
+                        {posts.map((post) => {
+                            return (
+                                <div className="col-md-3">
+                                    <h3>{post.title}</h3>
+                                    <p>{post.body}</p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
         </div>
     );
-}
+};
 
 export default App;
